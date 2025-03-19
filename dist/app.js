c602c88c52bb4f42c169ac310e82915615cb7958
Ubuntu <azureuser@lproj-17-03-25-VM.isjrdwdqlg5ebpvn52szhrpoyg.xx.internal.cloudapp.net>
2025-03-19 13:54:14 +0000

    Final-commit

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const mysql2_1 = __importDefault(require("mysql2"));
 const envFile = `.env.${process.env.NODE_ENV || "dev"}`;
 (0, dotenv_1.config)({ path: envFile }); // Load environment variables from .env file
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
+// Create a MySQL connection
+const connection = mysql2_1.default.createConnection({
+    host: "localhost", // or use your MySQL server's IP address
+    user: process.env.DB_USER, // Set this in your .env file
+    password: process.env.DB_PASSWORD, // Set this in your .env file
+    database: process.env.DB_NAME, // Set this in your .env file
+});
+// API Endpoint for DB Health Check
+app.get("/api/db-health", (req, res) => {
+    connection.query("SELECT 1", (err, results) => {
+        if (err) {
+            return res.status(500).json({ status: "error", message: err.message });
+        }
+        res.status(200).json({ status: "success", message: "Database is up", data: results });
+    });
+});
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world !!!!</h1>");
+    res.send("<h1>Hello world</h1>");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
-// ecosystem.config.js
 module.exports = {
   apps: [{
     name: "github-pipeline-test",
-    script: "dist/app.js",
+    script: "dist/app.js", // Use the compiled JS file from the dist directory
     instances: 1,
     exec_mode: "fork",
     env_dev: {
       NODE_ENV: "dev",
-      PORT: 8000
-      // Other environment variables
+      PORT: 8000,
+      DB_USER: "node_api",       // Add DB credentials for dev environment
+      DB_PASSWORD: "node_api@12345",
+      DB_NAME: "node_testdb"
     },
     env_prod: {
       NODE_ENV: "prod",
-      PORT: 5000
-      // Other environment variables
+      PORT: 5000,
+      DB_USER: "node_api",       // Add DB credentials for prod environment
+      DB_PASSWORD: "node_api@12345",
+      DB_NAME: "node_testdb"
     }
   }]
 }
