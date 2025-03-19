62b002dc044a1a898413a2f44e86388247664d55
Ubuntu <azureuser@lproj-17-03-25-VM.isjrdwdqlg5ebpvn52szhrpoyg.xx.internal.cloudapp.net>
2025-03-19 09:05:03 +0000

    commited

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 
 const envFile = `.env.${process.env.NODE_ENV || "dev"}`;
-config({ path: envFile }); // Load environment variables from.env file
+config({ path: envFile }); // Load environment variables from .env file
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "<h1>Test file deployed</h1>",
-  });
+  res.send("<h1>Test file deployed</h1>");
 });
 
 app.listen(PORT, () => {
